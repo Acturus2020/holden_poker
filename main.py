ea58c43e5bafcd4930ff26e42c63393ebc9526aa
Dmytro Lyapun <dlyapun@gmail.com>
2021-05-18 19:05:10 +0300

    feature/add-requests-library - Add url and base request

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import requests
+
+API_CURRENCY_URL = 'https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5'
+
 
 def main():
     print('Start Holden Poker')
+    response = requests.get(API_CURRENCY_URL)
+    print(response.json())
 
 if __name__ == '__main__':
     main()
