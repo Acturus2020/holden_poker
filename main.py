0743fcaa94f1b766cc34c5832bd02cd10ac09cc9
Dmytro Lyapun <dlyapun@gmail.com>
2021-05-18 20:01:12 +0300

    feature/no-task/add-get-public-ip-address - Get user ip address

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 import requests
+import publicip
 
 API_CURRENCY_URL = 'https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5'
+IP_ADDRESS_URL = "http://ip-api.com/json/"
+
+def get_user_address(ip_address):
+    url = IP_ADDRESS_URL.format(ip_address)
+    response = requests.get(url)
+    user_address = response.json()
+    print('user_address: ', user_address)
 
 
 def main():
+    get_user_address(publicip.get())
     print('Start Holden Poker')
     response = requests.get(API_CURRENCY_URL)
     currency_list = response.json()
